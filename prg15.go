b71979afa40d27960d25da7cc1b1924b94542a55
VatsalSolanki-01 <solanki.vatsal009@gmail.com>
2025-09-15 23:55:53 +0530

    consistent drops to create flood of progress

---
+++ prg15.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type Employee interface {
+	salary() float64
+	display()
+}
+
+type FullTime struct {
+	name       string
+	baseSalary float64
+	bonus      float64
+}
+
+func (f FullTime) salary() float64 {
+	return f.baseSalary + f.bonus
+}
+
+func (f FullTime) display() {
+	fmt.Println("name of the employee :- ", f.name)
+	fmt.Println("salary of the employee :- ", f.salary())
+}
+
+type PartTime struct {
+	name        string
+	HourlyRate  float64
+	HoursWorked float64
+}
+
+func (p PartTime) salary() float64 {
+	return p.HourlyRate * p.HoursWorked
+}
+
+func (p PartTime) display() {
+	fmt.Println("name of the employee :- ", p.name)
+	fmt.Println("salary of the employee :- ", p.salary())
+}
