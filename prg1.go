9ad88928ff6b5f9e433f9653df9c79ac900151e2
VatsalSolanki-01 <solanki.vatsal009@gmail.com>
2025-09-16 17:38:57 +0530

    consistent drops to create flood of progress

--- prg1.go
+++ prg1.go
@@ -1,14 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	r := Rectangle{length: 10, width: 5}
-	s := Square{side: 7}
-
-	fmt.Println("Rectangle Area:", area(r))
-	fmt.Println("Rectangle Perimeter:", perimeter(r))
-
-	fmt.Println("Square Area:", area(s))
-	fmt.Println("Square Perimeter:", perimeter(s))
+	ShapePlay()
 }
--- prg14.go
+++ prg14.go
@@ -1,8 +1,71 @@
 package main
 
+import "fmt"
+
+// slice of type shape
+var shapes []shape
+
+func ShapePlay() {
+	var input string
+	for {
+		fmt.Println("enter 1 to create shape ")
+		fmt.Println("enter 2 to display details of all shapes ")
+		fmt.Println("enter 3 to exit ")
+		fmt.Scan(&input)
+		if input == "3" {
+			break
+		} else {
+			ShapePlayOptions(input)
+		}
+	}
+}
+
+func ShapePlayOptions(input string) {
+	switch input {
+	case "1":
+		createShapes()
+	case "2":
+		displayShapes()
+	}
+}
+
+// to create shapes and append in the list of shapes slice
+func createShapes() {
+	var input string
+	fmt.Println("enter a to create Rectangle b to create Square")
+	fmt.Scan(&input)
+	switch input {
+	case "a":
+		var length float64
+		var width float64
+		fmt.Println("enter length:-")
+		fmt.Scan(&length)
+		fmt.Println("enter width:-")
+		fmt.Scan(&width)
+		var r Rectangle
+		shapes = append(shapes, r.createRectangle(length, width))
+
+	case "b":
+		var side float64
+		fmt.Println("enter side:-")
+		fmt.Scan(&side)
+		var s Square
+		shapes = append(shapes, s.createSquare(side))
+	}
+}
+
+// display shapes' details
+func displayShapes() {
+	for _, ob := range shapes {
+		ob.details()
+	}
+}
+
+// interface
 type shape interface {
 	area() float64
 	perimeter() float64
+	details()
 }
 
 func area(sh shape) float64 {
@@ -13,6 +76,11 @@ func perimeter(sh shape) float64 {
 	return sh.perimeter()
 }
 
+func details(sh shape) {
+	sh.details()
+}
+
+// Rectangle structure
 type Rectangle struct {
 	length float64
 	width  float64
@@ -26,6 +94,17 @@ func (r Rectangle) perimeter() float64 {
 	return 2 * (r.length + r.width)
 }
 
+func (r Rectangle) details() {
+	fmt.Println("Shape type = Rectangle")
+	fmt.Println("length :- ", r.length, "width :- ", r.width)
+}
+
+// constructor like method which returns the Rectangle
+func (r Rectangle) createRectangle(length_i, width_i float64) Rectangle {
+	return Rectangle{length: length_i, width: width_i}
+}
+
+// Square structure
 type Square struct {
 	side float64
 }
@@ -36,4 +115,14 @@ func (s Square) area() float64 {
 
 func (s Square) perimeter() float64 {
 	return 4 * s.side
+}
+
+// constructor like method which returns the Square object
+func (s Square) createSquare(side float64) Square {
+	return Square{side: side}
+}
+
+func (s Square) details() {
+	fmt.Println("Shape type = Square")
+	fmt.Println("side's length :- ", s.side)
 }
