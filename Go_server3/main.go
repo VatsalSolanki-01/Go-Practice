8427b41b9fb21e0c62a198526cdc57d9d181614b
VatsalSolanki-01 <solanki.vatsal009@gmail.com>
2025-09-27 12:40:49 +0530

    consistent drops to create flood of progress

--- Go_server3/main.go
+++ Go_server3/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -11,7 +12,7 @@ func main() {
 	http.HandleFunc("/students", getStudentsInfo)
 
 	//GET method to get info of specific student based on rollno
-	http.HandleFunc("/students", getStudentInfo)
+	http.HandleFunc("/student", getStudentInfo)
 	if err := http.ListenAndServe(":8081", nil); err != nil {
 		fmt.Println("error starting server")
 	}
@@ -33,5 +34,28 @@ func getStudentsInfo(w http.ResponseWriter, r *http.Request) {
 }
 
 func getStudentInfo(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method not supported", http.StatusMethodNotAllowed)
+	}
 
+	rollno := r.URL.Query().Get("rollno")
+
+	if rollno == "" {
+		http.Error(w, " rollno required ", http.StatusBadRequest)
+		return
+	}
+
+	roll, err := strconv.Atoi(rollno)
+	if err != nil {
+		http.Error(w, "invalid rollno ", http.StatusBadRequest)
+	}
+
+	for _, v := range studentList {
+		if v.Rollno == roll {
+			w.Header().Set("Content-type", "application/json")
+			json.NewEncoder(w).Encode(v)
+			return
+		}
+	}
+	http.Error(w, "student not found", http.StatusNotFound)
 }
